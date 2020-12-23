85c5c26630b34edd51ef19cf6f93f1f71e7b32c6
Bilal Ather <mbamnc@mail.umsl.edu>
2020-12-23 17:31:45 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import {
+  Map,
+  Popup,
+  LayerPanel,
+  Controls,
+  ContextMenu,
+  LayerStyler,
+  LayerPanelPage,
+  LayerPanelContent,
+  BasemapContainer,
+  VectorLayer,
+  DrawContainer
+} from '@bayer/ol-kit'
+import { fromLonLat } from 'ol/proj'
+import olFeature from 'ol/Feature'
+import olGeomPoint from 'ol/geom/Point'
+import olSourceVector from 'ol/source/Vector'
+
+import LayersIcon from '@material-ui/icons/Layers'
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      wipeOnRefresh: true
+    }
+  }
+  onMapInit = async (map) => {
+    // create a vector layer and add to the map
+    const url = 'https://g499yhptoj.execute-api.us-east-1.amazonaws.com/dev/locations'
+    const locations = await fetch(url).then(res => res.json())
+    console.log(locations)
+    const features = locations.map((location) => {
+      return new olFeature({
+        ...location,
+        geometry: new olGeomPoint(fromLonLat([location.long, location.lat]))
+      })
+    })
+    const layer = new VectorLayer({
+      title: 'User Locations',
+      source: new olSourceVector({
+        features
+      })
+    })
+
+    map.addLayer(layer)
+
+    const getLocations = async () => {
+      // const url = 'http://localhost:3000/locations'
+      const locations = await fetch (url).then(res => res.json())
+      console.log(locations)
+
+      const features = locations.map((location) => {
+        return new olFeature({
+          ...location,
+          geometry: new olGeomPoint(fromLonLat([location.long, location.lat]))
+        })
+      })
+
+      if (this.state.wipeOnRefresh) layer.getSource().clear()
+
+      layer.getSource().addFeatures(features)
+    }
+
+    setInterval(getLocations, 3000)
+
+    window.map = map
+  }
+
+  render () {
+    return (
+      <Map onMapInit={this.onMapInit} fullScreen>
+        <Popup />
+        <LayerPanel>
+          <LayerPanelPage tabIcon='Layer Styler'>
+            <LayerPanelContent style={{ padding: '0px', fontFamily: 'Roboto, Arial, sans-serif' }}>
+              <LayerStyler />
+            </LayerPanelContent>
+          </LayerPanelPage>
+          <LayerPanelPage label={'Draw'}>
+            <LayerPanelContent style={{ padding: '0px', fontFamily: 'Roboto, Arial, sans-serif' }}>
+              <DrawContainer style={{ position: 'relative', width: 'auto' }} />
+            </LayerPanelContent>
+          </LayerPanelPage>
+        </LayerPanel>
+        <ContextMenu />
+        <Controls />
+        <BasemapContainer />
+      </Map>
+    )
+  }
+}
+
+export default App
